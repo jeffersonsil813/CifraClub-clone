215f971116219b93a08f18c32a9f737d7b17cead
Jefferson Santos da Silva <jefferson.sil813@gmail.com>
2023-05-25 15:04:08 -0300

    remove scrollReveal library, add next-reveal library, alter main content width and use MostAccessed component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,27 +2,17 @@
 import { Header } from "@/components/Header";
 import { Highlights } from "@/components/Highlights";
 import { MusicStyles } from "@/components/MusicStyles";
-import scrollReveal from "scrollreveal";
-import { useEffect } from "react";
+import { MostAccessed } from "@/components/MostAccessed";
 
 export default function Home() {
-  useEffect(() => {
-    scrollReveal().reveal(".animateOnScroll", {
-      origin: "top",
-      distance: "30px",
-      duration: 2000,
-      delay: 200,
-      interval: 600,
-    });
-  }, []);
-
   return (
     <div className="h-screen flex flex-col">
       <Header />
-      <div className="w-[68%] mx-auto space-y-4">
+      <main className="w-[70%] mx-auto space-y-4">
         <MusicStyles />
         <Highlights />
-      </div>
+        <MostAccessed />
+      </main>
     </div>
   );
 }
