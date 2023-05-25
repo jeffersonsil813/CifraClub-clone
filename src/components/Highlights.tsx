01a62886cd6b454b15da8db6cb66f307b60a701c
Jefferson Santos da Silva <jefferson.sil813@gmail.com>
2023-05-25 15:05:27 -0300

    add animation on highlights

--- src/components/Highlights.tsx
+++ src/components/Highlights.tsx
@@ -4,37 +4,43 @@ import BestBluesRock from "../../public/assets/images/image2.png";
 import FreddieMercury from "../../public/assets/images/image3.png";
 import AsPatroas from "../../public/assets/images/image4.png";
 import FooFighters from "../../public/assets/images/image5.png";
+import { RevealList, RevealWrapper } from "next-reveal";
 
 export function Highlights() {
   return (
     <div className="flex flex-col gap-4">
-      <Image
-        className="rounded-lg animateOnScroll"
-        src={MarisaMonte}
-        alt="Marisa Monte"
-      />
-      <div className="flex flex-col md:flex-row gap-4">
+      <RevealWrapper origin="top" delay={200} duration={1200} distance="35px">
+        <Image className="rounded-lg" src={MarisaMonte} alt="Marisa Monte" />
+      </RevealWrapper>
+      <RevealList
+        interval={400}
+        origin="top"
+        delay={500}
+        duration={1200}
+        distance="35px"
+        className="flex flex-col lg:flex-row gap-4"
+      >
         <Image
-          className="rounded-lg animateOnScroll"
+          className="rounded-lg w-full"
           src={BestBluesRock}
           alt="Best of Blues and Rock"
         />
         <Image
-          className="rounded-lg animateOnScroll"
+          className="rounded-lg w-full"
           src={FreddieMercury}
           alt="Freddie Mercury"
         />
         <Image
-          className="rounded-lg animateOnScroll"
+          className="rounded-lg w-full"
           src={AsPatroas}
           alt="As Patroas Show Completo"
         />
         <Image
-          className="rounded-lg animateOnScroll"
+          className="rounded-lg w-full"
           src={FooFighters}
           alt="Foo Fighters - Best of You"
         />
-      </div>
+      </RevealList>
     </div>
   );
 }
