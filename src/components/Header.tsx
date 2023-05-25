0a6233ad134eaf12db4759e38a8cee806cf3d76b
Jefferson Santos da Silva <jefferson.sil813@gmail.com>
2023-05-25 16:51:16 -0300

    add responsive layout on Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import logo from "../../public/assets/images/logo.png";
-import { Search } from "lucide-react";
+import { Search, ChevronDown, ChevronUp } from "lucide-react";
+import { useState } from "react";
 
 export function Header() {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
-    <div className="flex items-center justify-center gap-16 py-1 px-2 border-b border-solid border-zinc-300/50">
+    <div className="flex flex-col lg:flex-row lg:items-center justify-center gap-1 lg:gap-16 py-1 px-6 border-b border-solid border-zinc-300/50">
       <div className="flex items-center gap-6">
         <Image width={110} height={110} src={logo} alt="Cifra Club logo" />
         <div className="flex items-center justify-between gap-2 pl-3 pr-1 py-1 border-solid border border-zinc-300 rounded-lg w-96">
@@ -21,7 +24,11 @@ export function Header() {
           </button>
         </div>
       </div>
-      <ul className="flex items-center gap-1">
+      <ul
+        className={`${
+          showMenu ? "flex flex-col" : "hidden"
+        } lg:flex lg:flex-row lg:items-center gap-1`}
+      >
         <a
           href="#"
           className="text-zinc-800 font-semibold text-base px-4 py-2 transition-colors duration-100 hover:bg-zinc-200/50 hover:rounded-lg"
@@ -46,13 +53,32 @@ export function Header() {
         >
           Mais
         </a>
+        <div className="border-0 lg:border-r lg:border-dashed lg:border-zinc-300 lg:h-8" />
         <a
           href="#"
-          className="text-zinc-800 font-semibold text-base border-dashed border-l border-zinc-300 px-4 py-2 transition-colors duration-100 hover:bg-zinc-200/50 hover:rounded-lg"
+          className="text-zinc-800 font-semibold text-base px-4 py-2 transition-colors duration-100 hover:bg-zinc-200/50 hover:rounded-lg"
         >
           Entrar
         </a>
       </ul>
+      <button
+        type="button"
+        className="cursor-pointer flex gap-1 items-center text-zinc-500 font-semibold text-sm px-4 py-2 transition-colors duration-100 hover:bg-zinc-200/50 hover:rounded-lg hover:text-orange-500 group lg:hidden"
+        onClick={() => setShowMenu((prevState) => !prevState)}
+      >
+        Menu
+        {showMenu ? (
+          <ChevronDown
+            className="text-zinc-500 group-hover:text-orange-500 transition-colors duration-100"
+            size={25}
+          />
+        ) : (
+          <ChevronUp
+            className="text-zinc-500 group-hover:text-orange-500 transition-colors duration-100"
+            size={25}
+          />
+        )}
+      </button>
     </div>
   );
 }
