71a6393fd6b52f2b4cdbceabf4c9ff6d76085b88
Jefferson Santos da Silva <jefferson.sil813@gmail.com>
2023-05-24 22:36:51 -0300

    create musicStyles component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import { Header } from "@/components/Header";
+import { MusicStyles } from "@/components/MusicStyles";
 
 export default function Home() {
   return (
     <div className="h-screen flex flex-col">
       <Header />
+      <div className="w-[68%] mx-auto">
+        <MusicStyles />
+      </div>
     </div>
   );
 }
---
+++ src/components/MusicStyles.tsx
@@ -0,0 +1,44 @@
+import { ChevronDown } from "lucide-react";
+
+export function MusicStyles() {
+  return (
+    <div className="flex gap-2 mt-4">
+      <a
+        href="#"
+        className="bg-zinc-800 text-zinc-50 border-solid border border-zinc-800 rounded-lg py-1 px-3 text-[13px] font-semibold flex items-center"
+      >
+        Todos
+      </a>
+      <a
+        href="#"
+        className="border-solid border border-zinc-300 rounded-lg py-1 px-3 text-[13px] font-semibold flex items-center transition-colors duration-100 hover:bg-zinc-200/50 hover:rounded-lg hover:border-transparent"
+      >
+        Rock
+      </a>
+      <a
+        href="#"
+        className="border-solid border border-zinc-300 rounded-lg py-1 px-3 text-[13px] font-semibold flex items-center transition-colors duration-100 hover:bg-zinc-200/50 hover:rounded-lg hover:border-transparent"
+      >
+        Gospel/Religioso
+      </a>
+      <a
+        href="#"
+        className="border-solid border border-zinc-300 rounded-lg py-1 px-3 text-[13px] font-semibold flex items-center transition-colors duration-100 hover:bg-zinc-200/50 hover:rounded-lg hover:border-transparent"
+      >
+        Sertanejo
+      </a>
+      <a
+        href="#"
+        className="border-solid border border-zinc-300 rounded-lg py-1 px-3 text-[13px] font-semibold flex items-center transition-colors duration-100 hover:bg-zinc-200/50 hover:rounded-lg hover:border-transparent"
+      >
+        MPB
+      </a>
+      <a
+        href="#"
+        className="border-solid border border-zinc-300 rounded-lg py-1 px-3 text-[13px] font-semibold flex items-center gap-1 transition-colors duration-100 hover:bg-zinc-200/50 hover:rounded-lg hover:border-transparent"
+      >
+        Mais <ChevronDown size={15} />
+      </a>
+    </div>
+  );
+}
