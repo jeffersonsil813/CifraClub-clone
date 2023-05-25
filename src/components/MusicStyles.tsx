3460414039d4f32e0cc0a301cdaa93ae84f25d41
Jefferson Santos da Silva <jefferson.sil813@gmail.com>
2023-05-25 15:06:43 -0300

    remove unnecessary code

--- src/components/MusicStyles.tsx
+++ src/components/MusicStyles.tsx
@@ -5,31 +5,31 @@ export function MusicStyles() {
     <div className="flex gap-2 mt-4 overflow-x-auto">
       <a
         href="#"
-        className="bg-zinc-800 text-zinc-50 border-solid border border-zinc-800 rounded-lg py-1 px-3 text-[13px] font-semibold flex items-center"
+        className="bg-zinc-800 text-zinc-50 border-solid border border-zinc-800 rounded-lg py-1 px-3 text-[13px] font-semibold"
       >
         Todos
       </a>
       <a
         href="#"
-        className="border-solid border border-zinc-300 rounded-lg py-1 px-3 text-[13px] font-semibold flex items-center transition-colors duration-100 hover:bg-zinc-200/50 hover:rounded-lg hover:border-transparent"
+        className="border-solid border border-zinc-300 rounded-lg py-1 px-3 text-[13px] font-semibold transition-colors duration-100 hover:bg-zinc-200/50 hover:rounded-lg hover:border-transparent"
       >
         Rock
       </a>
       <a
         href="#"
-        className="border-solid border border-zinc-300 rounded-lg py-1 px-3 text-[13px] font-semibold flex items-center transition-colors duration-100 hover:bg-zinc-200/50 hover:rounded-lg hover:border-transparent"
+        className="border-solid border border-zinc-300 rounded-lg py-1 px-3 text-[13px] font-semibold transition-colors duration-100 hover:bg-zinc-200/50 hover:rounded-lg hover:border-transparent"
       >
         Gospel/Religioso
       </a>
       <a
         href="#"
-        className="border-solid border border-zinc-300 rounded-lg py-1 px-3 text-[13px] font-semibold flex items-center transition-colors duration-100 hover:bg-zinc-200/50 hover:rounded-lg hover:border-transparent"
+        className="border-solid border border-zinc-300 rounded-lg py-1 px-3 text-[13px] font-semibold transition-colors duration-100 hover:bg-zinc-200/50 hover:rounded-lg hover:border-transparent"
       >
         Sertanejo
       </a>
       <a
         href="#"
-        className="border-solid border border-zinc-300 rounded-lg py-1 px-3 text-[13px] font-semibold flex items-center transition-colors duration-100 hover:bg-zinc-200/50 hover:rounded-lg hover:border-transparent"
+        className="border-solid border border-zinc-300 rounded-lg py-1 px-3 text-[13px] font-semibold transition-colors duration-100 hover:bg-zinc-200/50 hover:rounded-lg hover:border-transparent"
       >
         MPB
       </a>
